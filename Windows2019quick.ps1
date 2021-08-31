8f50e2ec7d6e9701dcc7f628715024eba31cc4fe
Jermu Virtanen <jermuv@googlemail.com>
2021-08-31 22:21:39 +0300

    uus filu

---
+++ Windows2019quick.ps1
@@ -0,0 +1,11 @@
+
+
+
+Labconfig
+
+$LabConfig=@{ DomainAdminName='LabAdmin'; AdminPassword='LS1setup!'; Prefix = 'WSLab-'; SwitchName = 'LabSwitch'; DCEdition='4' ; Internet=$false ;AdditionalNetworksConfig=@(); VMs=@()}
+
+$LabConfig.VMs += @{ VMName = 'Win2016Core'     ; Configuration = 'Simple'; ParentVHD = 'Win2016Core_G2.vhdx' ; MemoryStartupBytes= 512MB }
+$LabConfig.VMs += @{ VMName = 'Win2016Full'     ; Configuration = 'Simple'; ParentVHD = 'Win2016_G2.vhdx'     ; MemoryStartupBytes= 512MB }
+$LabConfig.VMs += @{ VMName = 'Win2019Core'     ; Configuration = 'Simple'; ParentVHD = 'Win2019Core_G2.vhdx' ; MemoryStartupBytes= 512MB }
+$LabConfig.VMs += @{ VMName = 'Win2019Full'     ; Configuration = 'Simple'; ParentVHD = 'Win2019_G2.vhdx'     ; MemoryStartupBytes= 512MB }
